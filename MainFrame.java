0ece1b909a70931491a9a7995885a55eb93dcec0
Ruben Seegers <seegersruben@gmail.com>
2024-04-23 10:14:09 +0200

    Basis HMI, verdient nog aandacht

--- MainFrame.java
+++ MainFrame.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class MainFrame extends JFrame implements ActionListener {

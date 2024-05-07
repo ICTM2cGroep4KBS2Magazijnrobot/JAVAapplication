9b2e2f3a06d672ea9a1587c9c5c1f19087ceeb6c
Naweed2023 <luxnoori@gmail.com>
2024-05-07 14:54:47 +0200

    Doos class gemaakt

---
+++ src/Doos.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Doos {
+    private ArrayList<Product> inhoud = new ArrayList<>();
+
+
+    public Doos(Product product) {
+        inhoud.add(product);
+    }
+
+    public Doos(Product product, Product product2) {
+        inhoud.add(product);
+        inhoud.add(product2);
+    }
+
+    public Doos(Product product, Product product2, Product product3) {
+        inhoud.add(product);
+        inhoud.add(product2);
+        inhoud.add(product3);
+    }
+
+    public ArrayList<Product> getInhoud() {
+        return inhoud;
+    }
+
+    public Product getProduct(int num) {
+        return inhoud.get(num);
+    }
+
+    public void setInhoud(int num, Product product) {
+        inhoud.set(num,product);
+    }
+}
